fd7724b85eab4e6d68dd1dccd4a677a0da817135
mikoto2000 <mikoto2000@gmail.com>
2024-08-01 17:54:08 +0900

    ホームに相対時間を追加。

--- src/functions/task/TaskHistoryItem.tsx
+++ src/functions/task/TaskHistoryItem.tsx
@@ -2,22 +2,33 @@ import { Box } from "@mui/material";
 
 import dayjs, { Dayjs } from "dayjs";
 import timezone from 'dayjs/plugin/timezone'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 import React from "react";
 
 dayjs.extend(timezone);
-dayjs.tz.setDefault('Asia/Tokyo');
+dayjs.extend(relativeTime);
 
 type TaskHistoryItemProps = {
   datetime: Dayjs
 };
 
 const machineTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const userLocale = navigator.language.split('-')[0];
+
+import(`dayjs/locale/${userLocale}`)
+  .then(() => {
+    dayjs.locale(userLocale);
+  })
+  .catch(() => { })
 
 export const TaskHistoryItem: React.FC<TaskHistoryItemProps> = ({ datetime }) => {
+
+  const datetimeTz = datetime.tz(machineTimeZone);
+
   return (
     <Box>
-      {datetime.tz(machineTimeZone).format()}
+      {datetimeTz.format()}{}({datetimeTz.fromNow()})
     </Box>
   )
 }
