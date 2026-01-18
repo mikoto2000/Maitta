680cdb9a7c36b4a31f73b2fbf0765f2bb0224b99
mikoto2000 <mikoto2000@gmail.com>
2026-01-18 02:59:47 +0000

    Added spring boot project template.

---
+++ src-spring-boot/src/main/java/dev/mikoto2000/maitta/MaittaApplication.java
@@ -0,0 +1,13 @@
+package dev.mikoto2000.maitta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaittaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaittaApplication.class, args);
+	}
+
+}
